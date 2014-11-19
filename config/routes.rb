3aa3bc962f3a5009bb6223b967213e61401395e5
YusukeKinoshita <y.underthetree23@gmail.com>
2014-11-20 03:39:54 +0900

    Make Sign In And Out Function

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Designgirls::Application.routes.draw do
-  resources :slides
-
-  resources :tutorials
-
   root  'about#index'
   match '/about', to:'about#index', via:'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
   resources :users
   resources :tutorials
   resources :slides
+  resources :sessions, only: [:new, :create, :destroy]
 end
