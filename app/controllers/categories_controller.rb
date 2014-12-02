d5d9728d3a6e023c37756260ec2483a31380733f
erirazapii <eri.razapii@gmail.com>
2014-12-03 02:14:37 +0900

    UI of mypage also created a bit.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,6 +5,7 @@ class CategoriesController < ApplicationController
   # GET /categories.json
   def index
     @categories = Category.all
+    @user = User.find(params[:id])
   end
 
   # GET /categories/1
