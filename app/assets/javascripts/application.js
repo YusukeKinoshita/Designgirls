ac873a117bcfc5d0485c95082f1ff935e27488b6
erirazapii <eri.razapii@gmail.com>
2014-12-03 22:09:33 +0900

    Made the frame of image circle

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,3 +3,19 @@
 //= require bootstrap
 //= require turbolinks
 //= require_tree .
+
+
+var $menuIcon = $('div.menu-icon');
+
+var flg = false;
+
+$menuIcon.click(function(){
+    var $menu = $('div.menu');
+    if(flg){
+        flg = false;
+        $menu.css('transform','translate(-' + 200 + 'px)');
+    } else {
+        flg = true;
+        $menu.css('transform','translate(' + 0 + 'px)');
+    }
+});
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Designgirls::Application.routes.draw do
   match '/about', to:'about#index', via:'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
+  match '/mypage', to: 'categories#mypage',     via: 'get'
   resources :users
   # resources :tutorials
   # resources :slides
