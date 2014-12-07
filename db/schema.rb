40280d5b96928b9b14907ca5ec2bb5553243bbec
erirazapii <eri.razapii@gmail.com>
2014-12-07 22:22:03 +0900

    show/index error fixed and gridforms added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141205074522) do
+ActiveRecord::Schema.define(version: 20141207085811) do
 
   create_table "categories", force: true do |t|
     t.string   "title"
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 20141205074522) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "category_id"
+    t.integer  "tutorial_id"
   end
 
   create_table "tutorials", force: true do |t|
@@ -86,5 +88,6 @@ ActiveRecord::Schema.define(version: 20141205074522) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
 
 end
