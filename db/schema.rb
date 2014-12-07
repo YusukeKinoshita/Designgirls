7a5575dbec724bb540c3128d3af3729aec88cb2e
YusukeKinoshita <y.underthetree23@gmail.com>
2014-12-08 00:49:19 +0900

    Complete favorite function for products

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141205074522) do
+ActiveRecord::Schema.define(version: 20141207102724) do
 
   create_table "categories", force: true do |t|
     t.string   "title"
     t.string   "image"
     t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "favorites", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "product_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
