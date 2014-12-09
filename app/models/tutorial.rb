2ea34debca7833c87af053118b33b15d39d3cede
YusukeKinoshita <y.underthetree23@gmail.com>
2014-12-09 18:07:36 +0900

    complete tutorial form

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -7,8 +7,8 @@ class Tutorial < ActiveRecord::Base
   has_many :categories, through: :tags
 
   validates :title, presence: true
-  validates :user_id, presence: true
-  validates :category_id, presence: true
+  # validates :user_id, presence: true
+  # validates :category_id, presence: true
   validates :description, presence: true
 
 
